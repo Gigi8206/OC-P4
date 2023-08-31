96979a3d6fb780f600b169caa1ab763cf158f23d
Aurelien lebray <aurelien.lebray@gmail.com>
2023-08-31 15:29:52 +0200

    Resolved last merge conflicts

--- controllers/tournament.py
+++ controllers/tournament.py
@@ -4,7 +4,6 @@ from controllers.input import Input
 from models.tournament import Tournament
 from models.tournament import TournamentManager
 from views.tournament import TournamentView as View
-from models.player import Player
 
 
 class TournamentController():
@@ -49,15 +48,9 @@ class TournamentController():
         if not tournament:
             self.view.display_tournament_not_found()
             return
-<<<<<<< HEAD
         
         # Continue to process tournament
         print(f"Welcome to { tournament }")
-=======
-
-        # Continue to process tournament
-        print(f"Welcome to {tournament}")
->>>>>>> e2febead8f5fceb3ec39c4f984fe75a52a1814f3
 
         # ...
 
@@ -70,11 +63,7 @@ class TournamentController():
             for index, player in enumerate(self.player_manager.players):
                 print(f"{index}: {player}")
             message = f"PLAYER {index}: Select your player "
-<<<<<<< HEAD
-            numero = Input.for_string(message).capitalize()
-=======
             numero = Input.for_integer(message)
->>>>>>> e2febead8f5fceb3ec39c4f984fe75a52a1814f3
             player = self.player_manager.players[int(numero)]
         return player
 
@@ -86,7 +75,3 @@ class TournamentController():
             player = self.choose_users(index)
             players.append(player)
         return players
-<<<<<<< HEAD
-
-=======
->>>>>>> e2febead8f5fceb3ec39c4f984fe75a52a1814f3
