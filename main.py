2e2606327b1b26f1638f7a4857550cdcdcd67cc9
Aurelien lebray <aurelien.lebray@gmail.com>
2023-10-05 18:17:18 +0200

    Avoid removing main

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 
 def main():
-    players = [Player(first_name="Test", last_name=f"Player { i }", birthday=datetime.now(), identifier="AA" + f"{i}" * 5) for i in range(1, 9)]
-    tournament = Tournament(name="Grand Tournoi", place="Paris", date=datetime.now(), players=players)
-    print(tournament)
-    # MainMenuView.app_title()
-    # main_menu_controller = MainMenuController()
-    # main_menu_controller.main_loop()
+    # players = [Player(first_name="Test", last_name=f"Player { i }", birthday=datetime.now(), identifier="AA" + f"{i}" * 5) for i in range(1, 9)]
+    # tournament = Tournament(name="Grand Tournoi", place="Paris", date=datetime.now(), players=players)
+    # print(tournament)
+    MainMenuView.app_title()
+    main_menu_controller = MainMenuController()
+    main_menu_controller.main_loop()
 
 if __name__ == '__main__':
     main()
